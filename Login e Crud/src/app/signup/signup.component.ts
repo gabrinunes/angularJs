961aa8609b278ded0b96f7be2bece3185be5c539
gabrinunes <gn_cunha@hotmail.com>
2017-04-04 22:51:54 -0300

    Add files via upload

--- Login e Crud/src/app/signup/signup.component.ts
+++ Login e Crud/src/app/signup/signup.component.ts
@@ -1,80 +1,74 @@
 import { Component, OnInit } from '@angular/core';
 import 'rxjs/Rx';
-import {BackandService} from 'angular2bknd-sdk';
+import { BackandService } from 'angular2bknd-sdk';
 
 @Component({
-  selector: 'app-signup',
-  templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.scss']
+    selector: 'app-signup',
+    templateUrl: './signup.component.html',
+    styleUrls: ['./signup.component.scss']
 })
 export class SignupComponent implements OnInit {
 
- 
-
-  ngOnInit() {
-  }
-
-  email:string = '';
-  firstName:string = '';
-  lastName:string = '';
-  signUpPassword: string = '';
-  confirmPassword: string = '';
-
-  constructor(private backandService:BackandService) {
 
 
-  }
-
-  public signUp() {
-    if (this.signUpPassword != this.confirmPassword){
-      alert('Passwords should match');
-      return;
+    ngOnInit() {
     }
-    var $obs = this.backandService.signup(this.email, this.signUpPassword, this.confirmPassword, this.firstName, this.lastName);
-    $obs.subscribe(                
-      data => {
-          alert('Sign up succeeded');
-          this.email = this.signUpPassword = this.confirmPassword = this.firstName = this.lastName = '';
-      },
-      err => {
-          this.backandService.logError(err)
-      },
-      () => console.log('Finish Auth'));
-  }
 
-  public socialSignin(provider) {
-    var $obs = this.backandService.socialSignin(provider);
-    $obs.subscribe(                
-        data => {
-            console.log('Sign up succeeded with:' + provider);           
-        },
-        err => {
-            this.backandService.logError(err)
-        },
-        () => console.log('Finish Auth'));
-  }
+    email: string = '';
+    firstName: string = '';
+    lastName: string = '';
+    Password: string = '';
+    confirmPassword: string = '';
 
-  public socialSignup(provider) {
-    var $obs = this.backandService.socialSignup(provider);
-    $obs.subscribe(                
-        data => {
-            console.log('Sign up succeeded with:' + provider);           
-        },
-        err => {
-            this.backandService.logError(err)
-        },
-        () => console.log('Finish Auth'));
-  }
+    constructor(private backandService: BackandService) {
 
-  // public inAppSocial(provider) {
-  //   var $obs = this.backandService.inAppSocial(provider);
-  //   $obs.subscribe(                
-  //       data => {
-  //           console.log('Sign up succeeded with:' + provider);           
-  //       },
-  //       err => {
-  //           this.backandService.logError(err)
-  //       },
-  //       () => console.log('Finish Auth'));
-  // }
-}
+
+    }
+
+    public signUp() {
+        if (this.Password != this.confirmPassword) {
+            alert('Passwords should match');
+            return;
+        }
+        this.backandService.signup(this.email, this.Password, this.confirmPassword, this.firstName, this.lastName);
+
+        alert('Sign up succeeded');
+        this.email = this.Password = this.confirmPassword = this.firstName = this.lastName = '';
+    }
+
+        //public socialSignin(provider) {
+        //var $obs = this.backandService.socialSignin(provider);
+        //$obs.subscribe(                
+        //data => {
+        //   console.log('Sign up succeeded with:' + provider);           
+        // },
+        //err => {
+        //    this.backandService.logError(err)
+        //  },
+        //    () => console.log('Finish Auth'));
+        //}
+
+        // public socialSignup(provider) {
+        //  var $obs = this.backandService.socialSignup(provider);
+        //   $obs.subscribe(                
+        // data => {
+        // console.log('Sign up succeeded with:' + provider);           
+        //  },
+        // err => {
+        //     this.backandService.logError(err)
+        // },
+        // () => console.log('Finish Auth'));
+        //}
+
+        // public inAppSocial(provider) {
+        //   var $obs = this.backandService.inAppSocial(provider);
+        //   $obs.subscribe(                
+        //       data => {
+        //           console.log('Sign up succeeded with:' + provider);           
+        //       },
+        //       err => {
+        //           this.backandService.logError(err)
+        //       },
+        //       () => console.log('Finish Auth'));
+        // }
+    }
